ca738a42167c8ce54c28c8ad53a2ff3f87b7add0
Ondrej Koutsky <ondrej.koutsky@invasys.com>
2020-12-07 22:55:05 +0100

    removing TODO:

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,2 +1,2 @@
-find_package(Boost 1.71.0 REQUIRED COMPONENTS system program_options) # TODO: decide if program_options will be used
+find_package(Boost 1.71.0 REQUIRED COMPONENTS system program_options)
 find_package(nlohmann_json 3.7.3 REQUIRED)
